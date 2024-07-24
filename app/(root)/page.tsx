c5ac724d9aa873686b6be3f2ae0b32ebcda65f7c
tannisthaneogy <tannisthaneogy1@gmail.com>
2024-07-24 13:23:12 +0530

    implement plaidlink and dwolla

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,23 +1,11 @@
 import HeaderBox from '@/components/HeaderBox'
-// import RecentTransactions from '@/components/RecentTransactions';
 import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 import { getLoggedInUser } from '@/lib/actions/user.actions';
-// import { getAccount, getAccounts } from '@/lib/actions/bank.actions';
 
 const Home = async ({ searchParams: { id, page } }: SearchParamProps) => {
   const currentPage = Number(page as string) || 1;
   const loggedIn = await getLoggedInUser();
-  // const accounts = await getAccounts({ 
-  //   userId: loggedIn.$id 
-  // })
-
-  // if(!accounts) return;
-  
-  // const accountsData = accounts?.data;
-  // const appwriteItemId = (id as string) || accountsData[0]?.appwriteItemId;
-
-  // const account = await getAccount({ appwriteItemId })
 
   return (
     <section className="home">
